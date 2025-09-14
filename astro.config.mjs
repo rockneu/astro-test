ced85cf8902f0f34abc628f3c9f11d3c64811487
rockneu <rock.neu@gmail.com>
2025-09-14 08:52:24 +0800

    astro_test 1st commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable the React integration
+  // integrations: [react()],
+  
+  // Enable the Tailwind integration
+  // integrations: [tailwind()],
+  
+  // Enable SSR mode
+  // output: 'server',
+  
+  // Enable static mode
+  output: 'static',
+});
