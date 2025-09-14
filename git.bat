81aa6da2171adcdf2101b9709e6cdff7de0f6d04
rockneu <rock.neu@gmail.com>
2025-09-14 19:04:44 +0800

    a

--- git.bat
+++ git.bat
@@ -1,17 +1,4 @@
-@echo off
-echo Adding all changes...
-git add .
-
-set /p commit_msg="Enter commit message: "
-if "%commit_msg%"=="" (
-    set commit_msg="update: automated commit"
-)
-
-echo Committing with message: %commit_msg%
-git commit -m "%commit_msg%"
-
-echo Pushing to remote repository...
-git push
-
-echo Push completed successfully!
+git add . 
 pause
+git commit -m "" 
+git push
