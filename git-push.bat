b56e20de7401cb6c6bc4bfba7029ff2e632ed52a
rockneu <rock.neu@gmail.com>
2025-09-14 19:11:45 +0800

    aa

--- git-push.bat
+++ git-push.bat
@@ -1,20 +1,3 @@
-@echo off
-echo === Git Auto Push Script ===
-echo Adding files...
-git add .
-
-echo.
-set /p msg=Enter commit message (or press Enter for default): 
-if "%msg%"=="" (
-    set msg="update: automated commit"
-)
-
-echo Committing with message: %msg%
-git commit -m %msg%
-
-echo Pushing to remote...
+git add . 
+git commit -m "aa"
 git push
-
-echo.
-echo === Push completed! ===
-pause
